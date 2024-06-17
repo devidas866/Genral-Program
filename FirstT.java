0c3a59fe2705d5dcfff792ea8ccfa11458882a09
Devidas Suresh Pophalnare <134959317+devidas866@users.noreply.github.com>
2024-06-17 17:56:11 +0100

    Add files via upload
    First commit

---
+++ FirstT.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class FirstT
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+System.out.println("Enter name here");
+String name=s.nextLine();
+System.out.println("welcome"+name+"!!!");
+}
+}
